2ff235029c98f7f288cc0e703b7a0e613bfc8853
Angelica Urbanelli s271114 <s271114@studenti.polito.it>
2020-06-12 19:28:50 +0200

    added first version of Catalogue

--- client/src/components/Catalogue.js
+++ client/src/components/Catalogue.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState} from 'react';
+import {Row, Col, Image} from "react-bootstrap";
 
 
-function Catalogue(){
-    return <></>;
+function Catalogue(props){
+    const [cats, setCats] = useState(["A", "B"]);
+    const [brands, setBrands] = useState([]);
+
+    return <Row>
+        <Col md={3}>
+
+        </Col>
+        <Col md={8}>
+            <VehiclesList vehicles={props.vehicles} cats={cats} brands={brands} />
+        </Col>
+    </Row>;
+}
+
+function VehiclesList(props){
+    return props.vehicles
+        .filter(v => props.cats.indexOf(v.category) >= 0 || props.brands.indexOf(v.brand) >= 0 )
+        .map(v => <VehicleRow key={v.id} vehicle={v}/>);
+}
+
+function VehicleRow(props){
+    return <Row>
+    <Col sm={4}>
+        <Image className={"img-fluid"} src="img/prova.png" rounded />
+    </Col>
+    <Col sm={4}>
+        A partire da {props.vehicle.price}
+    </Col>
+    <Col sm={4}>
+        Category: {props.vehicle.category}
+        {props.vehicle.brand} {props.vehicle.model}
+    </Col>
+        </Row>;
 }
 
 export default Catalogue;
