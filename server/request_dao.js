ea2a26e38e5fd4ce9b7b1096af1f0d92297ccbef
Angelica Urbanelli s271114 <s271114@studenti.polito.it>
2020-06-20 19:47:13 +0200

    added REST APIs and dao for retrieving number and price of available cars

--- server/request_dao.js
+++ server/request_dao.js
@@ -1,25 +1,26 @@
 const db = require("./db");
 const Vehicle = require('./vehicle');
 
-exports.getAvailableVehicles = function(request){
+exports.getAvailableVehicles = function (request) {
     return new Promise(((resolve, reject) => {
-        const sql = "SELECT * " +
-                    "FROM vehicles" +
-                    "WHERE Category = ? AND " +
-                    "VehicleId NOT IN (" +
-                                        "SELECT VehicleId " +
-                                        "FROM rentals " +
-                                        "WHERE ( ? >= DateFrom AND ? <= DateTo ) " +
-                                        "OR ( ? >= DateFrom AND ? <= DateTo ) " +
-                                    ")";
-        db.all(sql, [request.category, request.datein, request.datein, request.dateout, request.dateout], (err, rows) => {
-            if (err)
+        const sql = "SELECT COUNT(*) as number " +
+            "FROM vehicles " +
+            "WHERE Category = ? AND " +
+            "VehicleId NOT IN (" +
+            "SELECT VehicleId " +
+            "FROM rentals " +
+            "WHERE ( ? >= DateFrom AND ? <= DateTo ) " +
+            "OR ( ? >= DateFrom AND ? <= DateTo ) " +
+            ")";
+
+        db.get(sql, [request.category, request.datein, request.datein, request.dateout, request.dateout], (err, row) => {
+            if (err) {
                 reject(err);
-            else{
-                let number = rows.count();
+            } else {
+                let number = row.number;
                 let price = -1;
-                if(number > 0){
-                    price = createVehicle(row[0]).price;
+                if (number > 0) {
+                    price = createVehicle({Category: request.category}).price;
                 }
                 const numberAndPrice = {
                     number: number,
@@ -32,9 +33,33 @@ exports.getAvailableVehicles = function(request){
     }));
 }
 
-//TODO: add query to get the number of cars of a given category
+exports.getNumberByCategory = (category) => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT COUNT(*) AS number FROM vehicles WHERE Category = ?";
+        db.get(sql, [category], (err, row) => {
+            if (err)
+                reject(err);
+            else {
+                resolve(row.number);
+            }
+        });
+    });
+}
 
-const createVehicle = function(row){
+exports.getPreviousRentals = (id) => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT COUNT(*) as number FROM rentals WHERE UserId = ?";
+        db.get(sql, [id], (err, row) => {
+            if (err)
+                reject(err);
+            else {
+                resolve(row.number);
+            }
+        });
+    });
+}
+
+const createVehicle = function (row) {
     const id = row.VehicleId;
     const category = row.Category;
     const brand = row.Brand;
--- server/server.js
+++ server/server.js
@@ -4,15 +4,17 @@ const PORT = 3001;
 
 app = new express();
 
-const expireTime = 50*1000; //seconds
+const expireTime = 50000 * 1000; //seconds
 const jwtSecret = "notSoSecret";
 const jsonwebtoken = require("jsonwebtoken");
 const jwt = require("express-jwt");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const moment = require("moment");
 
 const userDao = require('./user_dao');
 const vehicleDao = require('./vehicle_dao');
+const requestDao = require('./request_dao');
 
 // Set-up logging
 app.use(morgan('tiny'));
@@ -32,18 +34,17 @@ app.get("/api/login", jwt({
     credentialsRequired: false
 }), (req, res) => {
     console.log("the user was already auth?");
-    if(req.user && req.user.userId){
+    if (req.user && req.user.userId) {
         console.log("UserId : " + req.user.userId);
         userDao.getNameById(req.user.userId)
             .then((response) => res.status(200).json({name: response}).send())
-            .catch(() => res.status(401).end() );
-    }
-    else res.status(401).end();
+            .catch(() => res.status(401).end());
+    } else res.status(401).end();
 });
 
 app.post("/api/login", (req, res) => {
-    console.log("server.js POST login req.body ");
-    console.log(req.body);
+    if (!req.body.username || !req.body.password)
+        res.status(401).end();
     userDao.getUserByUsername(req.body.username)
         .then((user) => {
             console.log("server.js POST login " + user);
@@ -53,17 +54,17 @@ app.post("/api/login", (req, res) => {
                 const token = jsonwebtoken.sign({userId: user.id}, jwtSecret, {expiresIn: expireTime});
                 res.cookie('token', token, {httpOnly: true, sameSite: true, maxAge: expireTime});
                 res.status(200).json({name: user.name}).send();
-            }
-            else
-            res.status(401).end();
+            } else
+                res.status(401).end();
         })
         .catch((err) => {
             console.log("POST api login");
             console.log(err);
-            res.status(401).end();});
+            res.status(401).end();
+        });
 });
 
-app.post("/api/logout", (req, res) =>{
+app.post("/api/logout", (req, res) => {
     res.clearCookie('token').end();
 });
 
@@ -79,5 +80,62 @@ app.use(jwt({
 }));
 
 
+app.get('/api/vehicles/request', (req, res) => {
+    //check again data
+    if (req.query) {
+        const request = req.query;
+        if (checkData(request)) {
+            Promise.all([requestDao.getAvailableVehicles(request), requestDao.getNumberByCategory(request.category), requestDao.getPreviousRentals(req.user.userId)])
+                .then((results) => {
+                    const numberAndPrice = results[0];
+                    if (numberAndPrice.number === 0) {
+                        res.json(numberAndPrice);
 
-app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
+                    } else {
+                        const previousRentals = results[2];
+                        const totalCars = results[1];
+                        let price = calculatePrice(request, numberAndPrice, totalCars, previousRentals);
+                        numberAndPrice.price = price;
+                        res.json(numberAndPrice);
+                    }
+                })
+                .catch(err => res.status(500).json({error: err}));
+            return;
+        }
+    }
+    res.status(500).json({error: "invalid data"});
+});
+
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
+
+
+checkData = (request) => {
+    if (request.datein && request.dateout && moment(request.datein).isAfter(moment()) && moment(request.datein).isSameOrBefore(request.dateout)) {
+        let regexCat = new RegExp("[A-E]");
+        if (regexCat.test(request.category) && +request.kms !== -1 && +request.age !== -1 && +request.others >= 0) {
+            return true;
+        }
+    }
+}
+
+calculatePrice = (request, numberAndPrice, totalCars, previousRentals) => {
+    const kms_percentage = 1 + (request.kms == 1 ? -5 : (request.kms == 2 ? 0 : +5)) / 100;
+    const age_percentage = 1 + (request.age == 1 ? +5 : (request.age == 2 ? 0 : +10)) / 100;
+    const others_percentage = 1 + (request.others > 0 ? 15 : 0) / 100;
+    const insurance_percentage = 1 + (request.insurance == 1 ? 20 : 0) / 100;
+    const prev_percentage = 1 + (previousRentals >= 3 ? -10 : 0) / 100;
+    const totalCars_percentage = 1 + (numberAndPrice.number / totalCars < 0.1 ? 10 : 0);
+    const days = moment(request.dateout).diff(moment(request.datein), 'days') + 1;
+
+    const price = numberAndPrice.price
+        * kms_percentage
+        * age_percentage
+        * others_percentage
+        * insurance_percentage
+        * prev_percentage
+        * totalCars_percentage
+        * days;
+
+    return price;
+}
